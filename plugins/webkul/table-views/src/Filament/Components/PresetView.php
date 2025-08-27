3b92ec08200fa19dee8601d0be4038b9d0d728fe
Suraj Kashyap <suraj.kashyap370@webkul.in>
2025-08-27 18:16:17 +0530

    fix the model related issue in save view filters.

--- plugins/webkul/table-views/src/Filament/Components/PresetView.php
+++ plugins/webkul/table-views/src/Filament/Components/PresetView.php
@@ -24,13 +24,10 @@ class PresetView extends Tab
 
     protected static mixed $cachedFavoriteTableViews;
 
-    /**
-     * @return array<string|int, Tab>
-     */
     public function getFavoriteTableViews(): mixed
     {
         return TableViewFavorite::query()
-            ->where('user_id', auth()->id())
+            ->where('user_id', filament()->auth()->id())
             ->get();
     }
 
@@ -100,9 +97,6 @@ class PresetView extends Tab
         return 'heroicon-o-lock-closed';
     }
 
-    /**
-     * @return string | array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | null
-     */
     public function getColor(): string|array|null
     {
         return $this->evaluate($this->color);
