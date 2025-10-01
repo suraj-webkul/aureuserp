c025377432276f032ded62497e3e6b52fa43b4ca
sandeepchawla <sandeep.chawla322@webkul.in>
2025-10-01 14:01:54 +0530

    make trait HasRecordNavigationTabs for showing record specific pages with cluster navigation

--- plugins/webkul/support/src/Filament/Widgets/RecordNavigationTabs.php
+++ plugins/webkul/support/src/Filament/Widgets/RecordNavigationTabs.php
@@ -1,20 +1,20 @@
 <?php
 
-namespace Webkul\Sale\Filament\Widgets;
+namespace Webkul\Support\Filament\Widgets;
 
 use Filament\Widgets\Widget;
 
 class RecordNavigationTabs extends Widget
 {
-    protected string $view = 'sales::filament.widgets.record-navigation-tabs';
+    protected string $view = 'support::filament.widgets.record-navigation-tabs';
 
     protected static bool $isLazy = false;
 
     protected int|string|array $columnSpan = 'full';
 
-    public $navigationItems;
+    public array $navigationItems = [];
 
-    public function mount($navigationItems): void
+    public function mount(array $navigationItems = []): void
     {
         $this->navigationItems = $navigationItems;
     }
